3a1266d8ee2a47e090f1d4670b59f67a8bbd15e1
jhonatavbrg <jhonatavbrg@gmail.com>
2021-03-26 01:32:20 -0300

    Removendo enunciado

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/ex_04.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/ex_04.js
@@ -1,7 +1,3 @@
-/*
-Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
-*/
-
 let value = 0;
 
 if (value > 0) {
