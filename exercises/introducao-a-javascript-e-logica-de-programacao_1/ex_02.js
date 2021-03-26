656d1c4f08389c17de4a4edb006537621b443340
jhonatavbrg <jhonatavbrg@gmail.com>
2021-03-26 01:07:05 -0300

    Finalizado segundo exercício de JavaScript utilizando condicionais

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/ex_02.js
@@ -0,0 +1,10 @@
+let numberOne = 20;
+let numberTwo = 30;
+
+if (numberOne === numberTwo) {
+  console.log('Os números são iguais!');
+} else if (numberOne > numberTwo) {
+  console.log('O maior número entre', numberOne, 'e', numberTwo, 'é:', numberOne);
+} else {
+  console.log('O maior número entre', numberOne, 'e', numberTwo, 'é:', numberTwo);
+}
