2083dca51e22b88289bd879cda41d1591f5b02fc
jhonatavbrg <jhonatavbrg@gmail.com>
2021-03-26 01:30:44 -0300

    Finalizado quarto exercício de JavaScript utilizando condicionais

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/ex_04.js
@@ -0,0 +1,13 @@
+/*
+Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+*/
+
+let value = 0;
+
+if (value > 0) {
+  console.log('O valor', value, 'é positivo!');
+} else if (value < 0) {
+  console.log('O valor', value, 'é negativo!');
+} else {
+  console.log('O valor digitado é 0');
+}
