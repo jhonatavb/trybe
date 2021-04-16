ac50abf3fc2e2ebc9d757cf07a08633a935cca5c
jhonatavbrg <jhonatavbrg@gmail.com>
2021-04-15 23:18:34 -0300

    Finalizado oitavo exercício

---
+++ exercises/ex_08.js
@@ -0,0 +1,9 @@
+let firstNumber = Math.floor((Math.random() + 1) * 50);
+let secondNumber = Math.floor((Math.random() + 1) * 50);
+let thirdNumber = Math.floor((Math.random() + 1) * 50);
+
+if (firstNumber % 2 === 0 || secondNumber % 2 === 0 || thirdNumber % 2 === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
